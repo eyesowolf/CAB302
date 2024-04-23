89cd76475ff6c0d6aaae951a56c7256cd7dc584d
Willem Nekker <willemnekker1@gmail.com>
2024-04-23 20:20:45 +1000

    changed mood-inpit-view.fxml
    - made it look a lot better

--- src/main/java/com/example/cab302/HelloApplication.java
+++ src/main/java/com/example/cab302/HelloApplication.java
@@ -17,7 +17,7 @@ public class HelloApplication extends Application {
     private void showHelloView(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 1280, 720); // Set width to 1280 and height to 720
+        Scene scene = new Scene(root, 1280, 800); // Set width to 1280 and height to 720
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -26,7 +26,7 @@ public class HelloApplication extends Application {
     public void showMoodInputView(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("moodInput-view.fxml"));
         Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 1280, 720); // Set width to 1280 and height to 720
+        Scene scene = new Scene(root, 1280, 800); // Set width to 1280 and height to 720
         stage.setTitle("Mood Input");
         stage.setScene(scene);
         stage.show();
