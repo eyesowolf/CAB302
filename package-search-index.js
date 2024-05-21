e78c4accce4d482a6cfac2a8950a1094cd03efde
eyesowolf <eyesowolf@users.noreply.github.com>
2024-05-21 02:16:23 +0000

    Deploying to gh-pages from @ eyesowolf/CAB302@0785aecedbfebb96847f860bcfabc8d031079d2b 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.cab302","l":"com.example.cab302"},{"m":"com.example.cab302","l":"com.example.cab302.controller"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.cab302","l":"com.example.cab302"},{"m":"com.example.cab302","l":"com.example.cab302.controller"},{"m":"com.example.cab302","l":"com.example.cab302.dbmodelling"}];updateSearchResults();
