ce6a3be9158e007957e1cc81d8bf42783d423e1d
ddavidclarkk <david.clark172@hotmail.com>
2024-05-24 13:14:10 +1000

    set up database settings read write

--- src/main/java/com/example/cab302/SettingsManager.java
+++ src/main/java/com/example/cab302/SettingsManager.java
@@ -1,32 +1,77 @@
 package com.example.cab302;
 
-import java.util.Properties;
+
+import java.util.BitSet;
 import java.io.*;
+import com.example.cab302.dbmodelling.SqliteUsersDAO;
+import com.example.cab302.dbmodelling.User;
 
 public class SettingsManager {
-    private static final String SETTINGS_FILE = "user.settings";  //file doesdnt exist, need to set up in db
-    private Properties settings = new Properties();
 
     public UserSettings loadSettings() {
         UserSettings userSettings = new UserSettings();
-        try (InputStream input = new FileInputStream(SETTINGS_FILE)) {
-            settings.load(input);
-            userSettings.setDarkModeEnabled(Boolean.parseBoolean(settings.getProperty("darkMode", "false")));
-            userSettings.setNotificationsEnabled(Boolean.parseBoolean(settings.getProperty("notifications", "true")));
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        SqliteUsersDAO usersDAO = new SqliteUsersDAO();
+        User currentUser = usersDAO.getUserById(3);  //number(3) needs to be changed to the id of the user that is currently logged in
+        byte[] prefs = currentUser.getPrefs();
+
+        boolean[] prefValues = byteSeperate(prefs);
+        userSettings.setTrackUsageEnabled(prefValues[0]);
+        userSettings.setFacialRecognitionEnabled(prefValues[1]);
+        userSettings.setDarkModeEnabled(prefValues[2]);
+        userSettings.setGameFeaturesEnabled(prefValues[3]);
+        userSettings.setAchievementFeaturesEnabled(prefValues[4]);
+        userSettings.setNotificationsEnabled(prefValues[5]);
         return userSettings;
     }
 
     public void saveSettings(UserSettings userSettings) {
-        settings.setProperty("darkMode", Boolean.toString(userSettings.isDarkModeEnabled()));
-        settings.setProperty("notifications", Boolean.toString(userSettings.isNotificationsEnabled()));
 
-        try (OutputStream output = new FileOutputStream(SETTINGS_FILE)) {
-            settings.store(output, "User Settings");
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        // below is blob data order
+        //     trackUsageCheckBox; !
+        //    facialRecognitionCheckBox; !
+        //    themeToggleButton; !
+        //    gameFeaturesCheckBox; !
+        //    achievementFeaturesCheckBox; !
+        //    notificationsCheckBox; !
+
+        SqliteUsersDAO usersDAO = new SqliteUsersDAO();
+        User currentUser = usersDAO.getUserById(3);  //number(3) needs to be changed to the id of the user that is currently logged in
+
+        byte[] prefs = currentUser.getPrefs();
+
+        boolean[] prefValues = byteSeperate(prefs);
+        prefValues[0] = userSettings.isTrackUsageEnabled();
+        prefValues[1] = userSettings.isFacialRecognitionEnabled();
+        prefValues[2] = userSettings.isDarkModeEnabled();
+        prefValues[3] = userSettings.areGameFeaturesEnabled();
+        prefValues[4] = userSettings.areAchievementFeaturesEnabled();
+        prefValues[5] = userSettings.isNotificationsEnabled();
+        prefs = byteCombine(prefValues);
+        currentUser.setPrefs(prefs);
+
+        usersDAO.updateUser(currentUser);
+
+    }
+
+    private static boolean[] byteSeperate(byte[] input) {
+        BitSet bits = BitSet.valueOf(input);
+        boolean[] output = new boolean[input.length * 8];
+        for (int i = bits.nextSetBit(0); i != -1; i = bits.nextSetBit(i+1)) {
+            output[i] = true;
         }
+        return output;
+    }
+
+    private static byte[] byteCombine(boolean[] input) {
+        byte[] output = new byte[input.length / 8];
+        for (int entry = 0; entry < output.length; entry++) {
+            for (int bit = 0; bit < 8; bit++) {
+                if (input[entry * 8 + bit]) {
+                    output[entry] |= (128 >> bit);
+                }
+            }
+        }
+
+        return output;
     }
 }
